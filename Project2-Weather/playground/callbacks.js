4f43e35a69f1597abbe1380ef48781a05a8ee5ed
Rezz88 <zzerep.88@hotmail.com>
2018-05-23 19:35:10 -0400

    Added callbacks.js

---
+++ Project2-Weather/playground/callbacks.js
@@ -0,0 +1,14 @@
+var getUser = (id, callback) => {
+    var user = {
+        id: id,
+        name: "Rezz",
+    };
+
+    setTimeout(() => {
+        callback(user);
+    }, 3000);
+};
+
+getUser(99, (user) => {
+    console.log(user);
+});
