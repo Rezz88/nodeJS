019148a6699de57577013fc092751738b35378de
Rezz88 <zzerep.88@hotmail.com>
2018-05-24 17:30:29 -0400

    Added more requests to app.js

--- Project2-Weather/app.js
+++ Project2-Weather/app.js
@@ -4,5 +4,7 @@ request({
     url: "https://maps.googleapis.com/maps/api/geocode/json?address=4171%20Esplanade%20Ave%20Montreal&key=AIzaSyD6zUWuE-kylZPq-B22zzAVjLmn8tW-tHA",
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
 });
