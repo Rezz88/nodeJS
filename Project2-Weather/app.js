48127f307e1a2381882af9568bead02701d47088
Rezz88 <zzerep.88@hotmail.com>
2018-05-27 18:45:24 -0400

    App.js setup with yargs

--- Project2-Weather/app.js
+++ Project2-Weather/app.js
@@ -1,7 +1,23 @@
 const request = require("request");
+const yargs = require("yargs");
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: "address",
+            discribe: "Address to fetch weather for",
+            string: true,
+        }
+    })
+    .help()
+    .alias("help", "h")
+    .argv;
+
+    var encodedAddress = encodeURIComponent(argv.a);
 
 request({
-    url: "https://maps.googleapis.com/maps/api/geocode/json?address=4171%20Esplanade%20Ave%20Montreal&key=AIzaSyD6zUWuE-kylZPq-B22zzAVjLmn8tW-tHA",
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyD6zUWuE-kylZPq-B22zzAVjLmn8tW-tHA`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
