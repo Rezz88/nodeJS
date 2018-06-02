18bd9d9d9db487a52c867308a4698b8239493932
Rezz88 <zzerep.88@hotmail.com>
2018-06-01 22:51:39 -0400

    Error handling

--- Project2-Weather/app.js
+++ Project2-Weather/app.js
@@ -20,7 +20,13 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyD6zUWuE-kylZPq-B22zzAVjLmn8tW-tHA`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
-});
+    if (error) {
+        console.log("Unable to connect to Google servers. Please try again.");
+    } else if (body.status === "ZERO_RESULTS") {
+        console.log("Unable to find address.");
+    } else if (body.status === "OK") {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+        console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+    }
+}); 
